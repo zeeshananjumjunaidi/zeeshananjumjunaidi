19bf9087f8c2a83e6771b655be1e61fb1dd87682
Zeeshan Anjum Junaidi <zeeshananjumjunaidi@gmail.com>
2020-12-18 17:15:38 +0300

    update

--- js/sketch.js
+++ js/sketch.js
@@ -1,9 +1,18 @@
 
-
 function setup(){
+    print(innerWidth,innerHeight);
     createCanvas(innerWidth,innerHeight);
+    rectMode(CENTER);
+    imageMode(CENTER);
+    textAlign(CENTER);
 }
 
 function draw(){
+    noCursor();
+    translate(width/2,height/2);
     background(15,20,20);
+    circle(mouseX-width/2,mouseY-height/2,10);
+    stroke(100);
+    noFill();
+     rect(0,0,width-100,height-150);
 }
