be07a81d83d0fef4aa666b6b1adf0a344aaece19
Zeeshan Anjum Junaidi <zeeshananjumjunaidi@gmail.com>
2020-12-18 18:46:45 +0300

    added earth

--- js/sketch.js
+++ js/sketch.js
@@ -18,9 +18,6 @@ function draw(){
     noCursor();
     // translate(width/2,height/2);
     background(15,20,20);
-    circle(mouseX-width/2,mouseY-height/2,10);
-    stroke(100);
-    noFill();
     //  rect(0,0,width-100,height-150);
 
      // earth image
