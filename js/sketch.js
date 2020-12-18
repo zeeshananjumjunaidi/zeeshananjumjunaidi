3273963b7b6c6ab9d8486df02a00ddeee777a3a8
Zeeshan Anjum Junaidi <zeeshananjumjunaidi@gmail.com>
2020-12-18 19:19:30 +0300

    update credentials

--- js/sketch.js
+++ js/sketch.js
@@ -1,35 +1,31 @@
 
 let img;
 
-function setup(){
-    print(innerWidth,innerHeight);
-    createCanvas(innerWidth,innerHeight,WEBGL);
+function setup() {
+    print(innerWidth, innerHeight);
+    createCanvas(innerWidth, innerHeight, WEBGL);
     rectMode(CENTER);
     imageMode(CENTER);
     textAlign(CENTER);
+
 }
 // start loading images
-function preload()
-{
-  // image texture from http://planetpixelemporium.com/
-  img = loadImage('assets/img/earth.diffuse.2k.jpg');
+function preload() {
+    // image texture from http://planetpixelemporium.com/
+    img = loadImage('assets/img/earth.diffuse.2k.jpg');
 }
-function draw(){
+function draw() {
     noCursor();
-    // translate(width/2,height/2);
-    background(15,20,20);
-    //  rect(0,0,width-100,height-150);
+    background(15, 20, 20);
+    // earth image
+    noStroke();
+    texture(img);
 
-     // earth image
-  noStroke();
-  texture(img);
-  
-  // allow user to drag, zoom, and move globe
-  orbitControl();
-  
-  //earth's rotation
-  rotateY(millis() / 10000);
-  
-  //earth
-  sphere(200);
+    // allow user to drag, zoom, and move globe
+    orbitControl();
+
+    //earth's rotation
+    rotateY(millis() / 10000);
+    //earth
+    sphere(100);
 }
