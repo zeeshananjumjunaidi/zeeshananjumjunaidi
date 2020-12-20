e105851b4f208e37442cc59b04668392534696ce
Zeeshan Anjum <zeeshananjumjunaidi@gmail.com>
2020-12-20 09:58:35 +0300

    added capstone project

---
+++ js/app.js
@@ -0,0 +1,35 @@
+
+jQuery.githubUser = function(username, callback) {
+    jQuery.getJSON("http://github.com/api/v1/json/" + username + "?callback=?", callback);
+  }
+  
+  jQuery.fn.loadRepositories = function(username) {
+    this.html("<span>Querying GitHub for " + username +"'s repositories...</span>");
+  
+    var target = this;
+    $.githubUser(username, function(data) {
+      var repos = data.user.repositories;
+      sortByNumberOfWatchers(repos);
+  
+      var list = $('<dl/>');
+      target.empty().append(list);
+      $(repos).each(function() {
+        list.append('<dt><a href="'+ this.url +'">' + this.name + '</a></dt>');
+        list.append('<dd>' + this.description + '</dd>');
+      });
+    });
+  
+    function sortByNumberOfWatchers(repos) {
+      repos.sort(function(a,b) {
+        return b.watchers - a.watchers;
+      });
+    }
+  };
+  $(document).ready(()=>{
+    const searchQueryURL = 'https://api.github.com/users/zeeshananjumjunaidi';
+
+       fetch(searchQueryURL)
+      .then(result => result.json())
+      .then(response => console.log(response))
+      .catch(err => console.log(err))
+  });
