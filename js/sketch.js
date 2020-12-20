b4aa7f63e9679ffdbd400fa1ad573e319512b7c7
Zeeshan Anjum Junaidi <zeeshananjumjunaidi@gmail.com>
2020-12-21 00:36:23 +0300

    minor changes

--- js/sketch.js
+++ js/sketch.js
@@ -5,6 +5,7 @@ let bg;
 let glow;
 let sat;
 // let defaultFont;
+let stars = [];
 function setup() {
 
     createCanvas(innerWidth, innerHeight, WEBGL);
@@ -12,7 +13,11 @@ function setup() {
     imageMode(CENTER);
     textAlign(CENTER);
     background(0);
-    sat = new Satellite(0, 150, 5);
+    sat = new Satellite(0, 250, 5);
+    let camera = p5.Camera;
+    for (let i = 0; i < random(100, 150); i++) {
+        stars.push([createVector(random(-innerWidth, innerWidth), random(-innerHeight, innerHeight), -400), random(0.5, 2)]);
+    }
 }
 // start loading images
 function preload() {
@@ -26,15 +31,28 @@ function draw() {
     if (!img || !clouds) {
         return;
     }
-
-
+    for (let i = 0; i < stars.length; i++) {
+        push();
+        if (deltaTime % 3 == 0) {
+            fill(random(255, sin(deltaTime)));
+        }
+        else if (deltaTime % 100 == 0) {
+            fill(100,100, (100+random(255, sin(deltaTime)))%255);
+        }
+        else {
+            fill(170, 170, 255)
+        }
+        translate(stars[i][0]);
+        circle(0, 0, stars[i][1]);
+        pop();
+    }
 
     // allow user to drag, zoom, and move globe
-    orbitControl();
+    //orbitControl();
     texture(glow);
     noStroke();
-    ellipse(0,0,260,260);
-    
+    ellipse(0, 0, 260, 260);
+
     directionalLight(255, 255, 255, -100, 45, -1);
     push();
     // earth Tilt
@@ -42,9 +60,9 @@ function draw() {
     //earth's rotation
     rotateY(millis() / 10000);
 
-    
+
     //earth
-    
+
     // earth image
     noStroke();
     texture(img);
@@ -84,8 +102,8 @@ class Satellite {
         this.mass = mass;
         this.radius = radius;
         this.moveVelocity = -1;
-        this.acceleration=createVector(-0.7,0);
-        this.velocity=createVector();
+        this.acceleration = createVector(-0.7, 0);
+        this.velocity = createVector();
     }
     draw() {
         push();
@@ -102,11 +120,11 @@ class Satellite {
 
             /* Gravitational force */
             let G = 1;
-            let M = 80;
+            let M = 180;
             var d = dist(this.x, this.y, obj.x, obj.y)
             let gravity_force = ((G * this.mass * M) / (sq(d)));
-            let gravity_force_x=0;
-            let gravity_force_y=0;
+            let gravity_force_x = 0;
+            let gravity_force_y = 0;
             if (obj.x != this.x) {
                 alpha = atan(abs((obj.y - this.y)) / abs((obj.x - this.x)));
                 gravity_force_x = gravity_force * cos(alpha);
@@ -115,14 +133,13 @@ class Satellite {
                 gravity_force_x = 0;
                 gravity_force_y = gravity_force;
             }
-            let dir = p5.Vector.sub(obj,createVector(this.x,this.y)).normalize();
-            let force = createVector(dir.x*gravity_force_x,dir.y*gravity_force_y);
+            let dir = p5.Vector.sub(obj, createVector(this.x, this.y)).normalize();
+            let force = createVector(dir.x * gravity_force_x, dir.y * gravity_force_y);
             this.acceleration.add(force);
             this.velocity.add(this.acceleration);
-            this.x+=this.velocity.x;
-            this.y+=this.velocity.y;
+            this.x += this.velocity.x;
+            this.y += this.velocity.y;
             this.acceleration.mult(0);
-     
         }
     }
 }
