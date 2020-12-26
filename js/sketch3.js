5d22815d7376a5e2870c5f8669d1bd1df264702a
Zeeshan Anjum <zeeshananjumjunaidi@gmail.com>
2020-12-26 18:31:16 +0300

    adde lat long normal dir

--- js/sketch3.js
+++ js/sketch3.js
@@ -34,7 +34,7 @@ $(document).ready(() => {
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
     camera.position.set(0, 3, 5);
-    camera.position.set(15, 0, 0);
+    camera.position.set(15, 0, 10);
     camera.lookAt(scene.position);
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
@@ -190,7 +190,7 @@ $(document).ready(() => {
     const curvePoints = curve.getPoints(50);
     const curveGeometry = new THREE.BufferGeometry().setFromPoints(curvePoints);
 
-    const curveMaterial = new THREE.LineBasicMaterial({ color: 0x00ff00 });
+    const curveMaterial = new THREE.LineBasicMaterial({ color: 0x00fff0 });
 
     // Create the final object to add to the scene
     const ellipse = new THREE.Line(curveGeometry, curveMaterial);
@@ -200,25 +200,60 @@ $(document).ready(() => {
 
     // scene.add(generateArc(13,13,13,17,17,17));
   
-    let v1 =new THREE.Vector3(2.25,2.25,0);
-    let v2 =new THREE.Vector3(-2.25,2.25,0);
+    /// LatLon test
+    // let v1 =new THREE.Vector3(2.25,2.25,0);
+    // let v2 =new THREE.Vector3(-2.25,2.25,0);
+    // let v3 =new THREE.Vector3((v1.x+v2.x)/2,(v1.y+v2.y)/2,(v1.z+v2.z)/2);
 
-    
+    // let nv1 =  getlSphericalNormalVector(v1,3).multiplyScalar(3);
+    // let nv2 =  getlSphericalNormalVector(v2,3).multiplyScalar(3);
+    // let nv3 =  getlSphericalNormalVector(v3,3).multiplyScalar(1);
 
-    scene.add(generateDot(v1.x,v1.y,v1.z));
-    scene.add(generateDot(v2.x,v2.y,v2.z));
+    let lines= [];
+    let randomPoints =[];
+    for(let i=0;i<30;i++){
+        let q2 = latlonToSphericalProjection(Math.random(0,180),Math.random(0,360),3);
+        let nq2 =  getlSphericalNormalVector(q2,3).normalize().multiplyScalar(4);
+        let l2 = addNewLine(q2,nq2,curveMaterial);
+        lines.push(l2);
+        scene.add(l2);
+    }
+    // USA 37.0902° N, 95.7129° W
+    // let q2 = latlonToSphericalProjection(37.0902,95.7129,3);
+    // let nq2 =  getlSphericalNormalVector(q2,3).normalize().multiplyScalar(4);
+    // let l2 = addNewLine(q2,nq2,curveMaterial);
+    // lines.push(l2);
+    // scene.add(l2);
+    // Pakistan
+    let q1 = latlonToSphericalProjection(30.3753,69.3451,1);
+    let nq1 =  getlSphericalNormalVector(q1,3).multiplyScalar(2);
+    let l = addNewLine(q1,nq1,curveMaterial);
+    lines.push(l);
+    scene.add(l);
+
+    // scene.add(addNewLine(v1,nv1,curveMaterial));
+    // scene.add(addNewLine(v2,nv2,curveMaterial));
+    // scene.add(addNewLine(v3,nv3,curveMaterial));
+
+    // scene.add(generateDot(v1.x,v1.y,v1.z));
+    // scene.add(generateDot(v2.x,v2.y,v2.z));
+    // scene.add(generateDot(v3.x,v3.y,v3.z));
+   
+
+
+    /// End of LatLon test
 
 
 
-    const points1 =  getSplineFromCoords([0,0,100,14]).spline.getPoints( 50 );
-    const geometry1 = new THREE.BufferGeometry().setFromPoints( points1 );
+    // const points1 =  getSplineFromCoords([0,0,100,14]).spline.getPoints( 50 );
+    // const geometry1 = new THREE.BufferGeometry().setFromPoints( points1 );
 
-    const material1 = new THREE.LineBasicMaterial( { color : 0xff0000 } );
+    // const material1 = new THREE.LineBasicMaterial( { color : 0xff0000 } );
 
-    // Create the final object to add to the scene
-    const curveObject1 = new THREE.Line( geometry1, material1 );
+    // // Create the final object to add to the scene
+    // const curveObject1 = new THREE.Line( geometry1, material1 );
 
-    scene.add(curveObject1)
+    // scene.add(curveObject1)
     // scene.add(generateDot(-2.25,-2.25,0));
     // scene.add(generateDot(2.25,-2.25,0));
 
@@ -242,9 +277,11 @@ $(document).ready(() => {
         requestAnimationFrame(animate);
 
         earthMesh.rotation.y += 0.006;
+        for(let i=0;i<lines.length;i++){
+            lines[i].rotation.y += 0.006;
+        }
         earthCloudMesh.rotation.y += 0.005;
         pivotPoint.rotation.y += 0.002;
-
         // moonMesh.rotateAround(earthMesh.position);
         controls.update();
         renderer.render(scene, camera);
@@ -262,52 +299,76 @@ function generateDot(x1,y1,z1){
     return dot;
 }
 
-// Credit: https://medium.com/@xiaoyangzhao/drawing-curves-on-webgl-globe-using-three-js-and-d3-draft-7e782ffd7ab
-function getSplineFromCoords(coords) {
-    const startLat = coords[0];
-    const startLng = coords[1];
-    const endLat = coords[2];
-    const endLng = coords[3];
-    console.log(startLat,startLng);
-    // start and end points
-    const start = coordinateToPosition(startLat, startLng, GLOBE_RADIUS);
-    const end = coordinateToPosition(endLat, endLng, GLOBE_RADIUS);
+// // Credit: https://medium.com/@xiaoyangzhao/drawing-curves-on-webgl-globe-using-three-js-and-d3-draft-7e782ffd7ab
+// function getSplineFromCoords(coords) {
+//     const startLat = coords[0];
+//     const startLng = coords[1];
+//     const endLat = coords[2];
+//     const endLng = coords[3];
+//     console.log(startLat,startLng);
+//     // start and end points
+//     const start = coordinateToPosition(startLat, startLng, GLOBE_RADIUS);
+//     const end = coordinateToPosition(endLat, endLng, GLOBE_RADIUS);
    
-    // altitude
-    const altitude = clamp(start.distanceTo(end) * .75, CURVE_MIN_ALTITUDE, CURVE_MAX_ALTITUDE);
+//     // altitude
+//     const altitude = clamp(start.distanceTo(end) * .75, CURVE_MIN_ALTITUDE, CURVE_MAX_ALTITUDE);
     
-    // 2 control points
-    // const interpolate = geoInterpolate([startLng, startLat], [endLng, endLat]);
-    const midCoord1 = start.sub(end).multiplyScalar (0.25);
-    const midCoord2 = end.sub(start).multiplyScalar (0.75);
-    // const midCoord2 = interpolate(0.75);
-    const mid1 = coordinateToPosition(midCoord1.x,midCoord1.y, GLOBE_RADIUS + altitude);
-    const mid2 = coordinateToPosition(midCoord2.x,midCoord2.y, GLOBE_RADIUS + altitude);
+//     // 2 control points
+//     // const interpolate = geoInterpolate([startLng, startLat], [endLng, endLat]);
+//     const midCoord1 = start.sub(end).multiplyScalar (0.25);
+//     const midCoord2 = end.sub(start).multiplyScalar (0.75);
+//     // const midCoord2 = interpolate(0.75);
+//     const mid1 = coordinateToPosition(midCoord1.x,midCoord1.y, GLOBE_RADIUS + altitude);
+//     const mid2 = coordinateToPosition(midCoord2.x,midCoord2.y, GLOBE_RADIUS + altitude);
    
-    return {
-      start,
-      end,
-      spline: new THREE.CubicBezierCurve3(start, mid1, mid2, end)
-    };
-  }
+//     return {
+//       start,
+//       end,
+//       spline: new THREE.CubicBezierCurve3(start, mid1, mid2, end)
+//     };
+//   }
 
-  function latLonTo3d(lat,lon,alt){
-    let rad = 3;//     
-    let x = rad* Math.cos(lat) * Math.sin(lon);
-    let y = rad* Math.sin(lat) * Math.sin(lon);
-    let z = rad* Math.cos(lon);
-    return 
-  }
- function coordinateToPosition(lat, lng, radius) {
-    const phi = (90 - lat) * DEGREE_TO_RADIAN;
-    const theta = (lng + 180) * DEGREE_TO_RADIAN;
+//   function latLonTo3d(lat,lon,alt){
+//     let rad = 3;//     
+//     let x = rad* Math.cos(lat) * Math.sin(lon);
+//     let y = rad* Math.sin(lat) * Math.sin(lon);
+//     let z = rad* Math.cos(lon);
+//     return 
+//   }
+//  function coordinateToPosition(lat, lng, radius) {
+//     const phi = (90 - lat) * DEGREE_TO_RADIAN;
+//     const theta = (lng + 180) * DEGREE_TO_RADIAN;
   
-    return new THREE.Vector3(
-      - radius * Math.sin(phi) * Math.cos(theta),
-      radius * Math.cos(phi),
-      radius * Math.sin(phi) * Math.sin(theta)
-    );
-  }
-  function clamp(num, min, max) {
-    return num <= min ? min : (num >= max ? max : num);
-  }
+//     return new THREE.Vector3(
+//       - radius * Math.sin(phi) * Math.cos(theta),
+//       radius * Math.cos(phi),
+//       radius * Math.sin(phi) * Math.sin(theta)
+//     );
+//   }
+//   function clamp(num, min, max) {
+//     return num <= min ? min : (num >= max ? max : num);
+//   }
+
+function getlSphericalNormalVector(vec){
+    let _v = new THREE.Vector3(vec.x*2,vec.y*2,vec.z*2);// differentiate
+    let mag = Math.sqrt(Math.pow(_v.x,2),Math.pow(_v.y,2),Math.pow(_v.z,2));
+    if(mag==0)mag=1;
+    return new THREE.Vector3(_v.x/mag,_v.y/mag,_v.z/mag);
+}
+function addNewLine(p1,p2,mtl){
+        
+    const points2 = [];
+    points2.push( p1 );
+    // points2.push( v2 );
+    points2.push( p2 );
+
+    const geometry2 = new THREE.BufferGeometry().setFromPoints( points2 );
+    return new THREE.Line( geometry2, mtl );
+}
+function latlonToSphericalProjection(lat,lon,alt){
+    // ρ = altitude + radius of the planet
+    let x = Math.cos(lat) * Math.cos(lon) * alt
+    let y = Math.cos(lat) * Math.sin(lon) * alt
+    let z = Math.sin(lat) * alt // z is 'up'
+    return new THREE.Vector3(x,y,z);
+}
