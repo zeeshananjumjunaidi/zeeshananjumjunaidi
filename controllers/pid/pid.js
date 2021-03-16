eaf0988bbffe23afef9151771384040267bde7a5
Zeeshan Anjum <zeeshananjumjunaidi@gmail.com>
2021-03-16 11:37:34 +0300

    updated PID controller Class

--- controllers/pid/pid.js
+++ controllers/pid/pid.js
@@ -1,25 +1,65 @@
-class PID{
-    constructor(kP=0.2,kI=0.0,kD=0.0,current_time=undefined){
-        this.kP=kP;
-        this.kD=kD;
-        this.kI=kI;
-        this.sample_time = 0.0;
-        this.current_time = current_time?current_time?Date.now();
-        this.last_time = this.current_time;
+class PID {
+    constructor(kP = 0.2, kI = 0.0, kD = 0.0, currentTime = undefined) {
+        this.kP = kP;
+        this.kD = kD;
+        this.kI = kI;
+        this.sampleTime = 0.0;
+        this.currentTime = currentTime ? currentTime : Date.now();
+        this.lastTime = this.currentTime;
+        this.lastError = 0.0;
         this.output = 0.0;
         this.clear();
     }
-    clear(){
-        this.setPoint= 0.0;
+    clear() {
+        this.setPoint = 0.0;
         this.pTerm = 0.0;
         this.iTerm = 0.0;
         this.dTerm = 0.0;
-        
+
         // Windup Guard
         // https://en.wikipedia.org/wiki/Integral_windup
-        this. initError=0.0;
+        this.initError = 0.0;
         this.windupGuard = 10.0;
         this.output = 0.0;
     }
-
+    update(feedbackValue, currentTime = undefined) {
+        /* PID UPDATE
+         u(t) = K_p e(t) + K_i \int_{0}^{t} e(t)dt + K_d {de}/{dt}
+         */
+        let error = this.setPoint - feedbackValue
+        this.currentTime = currentTime ? currentTime : Date.now()
+        let deltaTime = this.currentTime - this.lastTime;
+        let deltaError = error - this.lastTime;
+        if (deltaTime >= this.sampleTime) {
+            this.pTerm = this.kP * error;
+            this.iTerm = error * deltaTime;
+            if (this.iTerm < -this.windupGuard) {
+                this.iTerm = -this.windupGuard
+            } else if (this.iTerm > this.windupGuard) {
+                this.iTerm = this.windupGuard;
+            }
+            this.dTerm = 0.0;
+            if (deltaTime >0){
+                this.dTerm = deltaError / deltaTime
+            }
+            this.lastTime  = this.currentTime
+            this.lastError = error;
+            this.output = this.pTerm + (this.kI*this.iTerm) + (this.kD*this.dTerm);
+        }
+    }
+    setKp(proportionalGain){
+        this.kP = proportionalGain;
+    }
+    setKi(integralGain){
+        this.kI = integralGain;
+    }
+    setKd(differentialGain){
+        this.kD = differentialGain;
+    }
+    setWindup(windup){
+        this.windupGuard = windup
+    }
+    setSampleTime(sampleTime){
+        this.sampleTime =sampleTime;
+    }
 }
