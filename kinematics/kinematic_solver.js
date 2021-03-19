494862af1fef7ac7a7a5612de72230affe3b124f
Zeeshan Anjum Junaidi <zeeshananjumjunaidi@gmail.com>
2021-03-19 23:57:06 +0300

    updated arm model, added axis model

---
+++ kinematics/kinematic_solver.js
@@ -0,0 +1,12 @@
+
+class KinematicSolver{
+
+    constructor(roboticArm){
+        this.roboticArm = roboticArm;
+    }
+
+    getDesiredAngles(x,y,z){
+        //TODO: return desired angles.
+    }
+
+}
--- kinematics/roboticArm.js
+++ kinematics/roboticArm.js
@@ -1,15 +1,15 @@
 class RoboticArm{
 
     constructor(sceneRef){
-        this.armBase; // from arm1 to Base = 61.043
-        this.arm1; // Length = 4.958
+        this.armBase; // from endEffector to Base = 61.043
+        this.endEffector; // Length = 4.958
         this.arm2; // Length = 24.357
         this.arm3; // Length = 28.534
 
         this.groundLevel = 1.941; // armBaseFromGround = 1.941
         this.arm3Len = 28.534;
         this.arm2Len = 24.357;
-        this.arm1Len = 4.958;
+        this.endEffectorLen = 4.958;
         this.armFullLength = 61.043;
         this.targetPos = new THREE.Vector3();
         this.sceneRef=sceneRef;
@@ -24,8 +24,8 @@ class RoboticArm{
                     //  console.log(child.name);       
                     if(child.name=='Base'){
                         this.armBase =child;
-                    }else if(child.name=='Arm1'){
-                        this.arm1=child;
+                    }else if(child.name=='endEffector'){
+                        this.endEffector=child;
                     }else if(child.name=='Arm2'){
                         this.arm2=child;
                     }else if(child.name=='Arm3'){
@@ -43,12 +43,12 @@ class RoboticArm{
 
     }
     distanceToTarget(){
-        return this.arm1.position.distanceTo(this.targetPos);
+        return this.endEffector.position.distanceTo(this.targetPos);
     }
     randomAnimate(dt){
-        if(this.arm1&&this.arm2&&this.arm3&&this.armBase){
+        if(this.endEffector&&this.arm2&&this.arm3&&this.armBase){
         this.armBase.rotation.y = Math.sin(dt*0.0005);
-        this.arm1.rotation.x = Math.cos(dt*0.0005);
+        this.endEffector.rotation.x = Math.cos(dt*0.0005);
         this.arm2.rotation.x = Math.cos(dt*0.0005);
         this.arm3.rotation.x = Math.cos(dt*0.0005);}    
     }
