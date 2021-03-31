9ab685b3223a8807c0299d5b538b02921ff85e17
Zeeshan Anjum <zeeshananjumjunaidi@gmail.com>
2021-03-31 16:20:00 +0300

    added rotation matrix

--- kinematics/pose.js
+++ kinematics/pose.js
@@ -1,10 +1,32 @@
 
 
-class Pose{
-    constructor(position,rotation,jointObj,pose){
-        this.pose=pose;
+class Pose {
+    constructor(position, rotation, jointObj, pose) {
+        this.pose = pose;
         this.startOffset = jointObj.position;
-        this.position=position;
+        this.position = position;
         this.rotation = rotation;
+    }
+    rotateAroundZ(a) {
+        return math.matrix([[Math.cos(a), -Math.sin(a), 0],
+        [Math.sin(a), Math.cos(a), 0],
+        [0, 0, 1]]);
+    }
+    rotateAroundY(a) {
+        return  math.matrix([[Math.cos(a), 0, Math.sin(a)],
+        [0, 1, 0],
+        [-Math.sin(a), 0, Math.cos(a)]]);
+    }
+    rotateAroundX(a) {
+        return  math.matrix([
+            [1, 0, 0],
+            [0, Math.cos(a), -Math.sin(a)],
+            [0, Math.sin(a), Math.cos(a)]]);
+    }
+    getFullRotationMatrix(a){
+        return math.multiply(math.multiply(
+            this.rotateAroundX(a),
+            this.rotateAroundY(a)),
+            this.rotateAroundZ(a));
     }
 }
--- kinematics/roboticArm.js
+++ kinematics/roboticArm.js
@@ -190,6 +190,11 @@ class RoboticArm {
             /*
             Pi = Pi-1 + rotate(Di,Pi-1,SumOf(Ak, where k=0 to i-1))
             */
+
+           for(let i=0;i<this.poses.length;i++){
+            let p = this.poses[i];
+            break;
+           }
         }else{
             // console.log("unreachable");
         }
