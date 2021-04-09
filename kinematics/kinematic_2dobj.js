1ebbdedb7b0db7e05d589200bd8be89ebc7a9a41
Zeeshan Anjum Junaidi <zeeshananjumjunaidi@gmail.com>
2021-04-09 23:41:52 +0300

    draw some visualization of angle and position-a

--- kinematics/kinematic_2dobj.js
+++ kinematics/kinematic_2dobj.js
@@ -6,7 +6,7 @@ class Kinematic2DObject {
         this.index = index;
         this.segmentCount=segmentCount;
         this.segments = [];
-        this.base =new p5.Vector(width/2,height);
+        this.base =new p5.Vector(width/2,height-50);
         this.segments.push(Segment.createRootInstance(300,300,Math.PI/2,segmentSize,-1));
         for(let i=1;i<segmentCount;i++){
             this.segments.push(Segment.createSegment(Math.PI,segmentSize,i));
@@ -20,6 +20,7 @@ class Kinematic2DObject {
         this.target.y = y;
     }
     update() {
+        line(0,height-50,width,height-50);
         let total = this.segments.length;
         let end = this.segments[total-1];
         end.follow(mouseX,mouseY);
@@ -39,5 +40,23 @@ class Kinematic2DObject {
         for(let i=0;i<this.segmentCount;i++){
             this.segments[i].show();
         }
+        this.drawDebug();
+    }
+    drawDebug(){
+        noFill();
+        let eeBx=this.segments[this.segmentCount-1].b.x;
+        let eeBy=this.segments[this.segmentCount-1].b.y;
+        let groundVector = new p5.Vector(width/2,height-50);
+        let vc = p5.Vector.sub(new p5.Vector(eeBx,eeBy),groundVector);
+        let angle = Math.atan2(groundVector.y-eeBy,groundVector.x-eeBx);
+        line(eeBx,eeBy,
+            eeBx,height-50);
+        line(0,this.segments[this.segmentCount-1].b.y,eeBx,
+            eeBy);
+        // arc(width/2,height-50,vc.mag()*2,vc.mag()*2,-180,0);
+
+        arc(width/2,height-50,vc.mag()*2,vc.mag()*2,-180,180+angle*180/Math.PI);
+
+
     }
 }
