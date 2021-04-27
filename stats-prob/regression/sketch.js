86263e1a6f8f6d2b9036386e5ce56ca169f29774
Zeeshan Anjum <zeeshananjumjunaidi@gmail.com>
2021-04-27 15:54:57 +0300

    fixed regression line slope and intercept

--- stats-prob/regression/sketch.js
+++ stats-prob/regression/sketch.js
@@ -3,7 +3,7 @@
 const width = window.innerWidth;
 const height = window.innerHeight;
 const divider = 40;
-const PARTICLE_COUNT = 2;
+const PARTICLE_COUNT = 5;
 let data = [];
 class Line{
     constructor(a,b,c,d){
@@ -31,8 +31,7 @@ function setup() {
         }
     }
     regressionLine = new Line(-width2,-height2,width2,height2);
-    LRModel = calculateLR();
-    console.log(LRModel);
+    LRModel = calculateLR(); 
 
 }var LRModel;
 function draw() {
@@ -45,16 +44,36 @@ function draw() {
     for(let i=0;i<data.length;i++){
         circle(data[i][0],data[i][1],2);
     }
-    drawRegressionLine();
-    drawError();
+    // drawRegressionLine();
+    // drawError();
     if(LRModel){
-        // y = (x * LRModel.slope) + LRModel.intercept
-        let x = 1;
-        let y=  (x * LRModel.slope) + LRModel.intercept;
-        let x1 = 200;
-        let y1=  (x1 * LRModel.slope) + LRModel.intercept;
-        circle(x,y,15);
-        circle(x1,y1,15);
+        strokeWeight(2);
+        stroke(0,255,50);
+        fill(0)
+        let endIndex=LRModel[0].length-1;
+        for(let i=0;i<LRModel[0].length;i++){
+            circle(LRModel[0][i],LRModel[1][i],5);
+        }
+        let x1=LRModel[0][0];
+        let y1=LRModel[1][0];
+        let x2=LRModel[0][endIndex];
+        let y2=LRModel[1][endIndex];
+        let dFactor = Math.sqrt(Math.pow(x2-x1,2)+Math.pow(y2-y1,2));
+        let dx = (x2-x1)/dFactor;
+        let dy = (y2-y1)/dFactor;
+        let xn1 = -dx*width/2;
+        let yn1 = -dy*height/2;
+        let xn2 = dx*width/2;
+        let yn2 = dy*height/2;
+
+        // regressionLine.x1=xn1;
+        // regressionLine.y1=yn1;
+        // regressionLine.x2=xn2;
+        // regressionLine.y2=yn2;
+        stroke(255,0,255);
+        strokeWeight(3)
+        line(xn1,yn1,xn2,yn2);
+    
     }
 }
 
@@ -82,7 +101,19 @@ function calculateLR(){
         model['intercept'] = (sumY - model.slope * sumX)/n;
         model['r2'] = Math.pow((n*sumXy - sumX*sumY)/Math.sqrt((n*sumXx-sumX*sumX)*(n*sumYy-sumY*sumY)),2);
 
-        return model;
+        var result_values_x = [];
+        var result_values_y = [];
+        let m = model['slope'];
+        let b = model['intercept'];
+        for (var v = 0; v< n; v++) {
+            x = data[v][0];
+            y = x * m + b;
+            result_values_x.push(x);
+            result_values_y.push(y);
+        }
+    
+        return [result_values_x, result_values_y];
+       // return model;
 }
 function drawRegressionLine(){
     stroke(255,0,0);
@@ -90,6 +121,7 @@ function drawRegressionLine(){
 }
 function drawError(){
     strokeWeight(0.5);
+    stroke(255,255,0);
     for(let i=0;i<data.length;i++){
         let d= data[i];
         let y = d[1];
@@ -102,4 +134,18 @@ function drawError(){
         line(x,y,d[0],d[1]);
         }
     }
+}
+function mousePressed(){
+    let x=mouseX-width2;
+    let y=mouseY-height2;
+    let removed=false;
+    for(let i=0;i<data.length;i++){
+        if (dist(x,y,data[i][0],data[i][1])<20){
+            data.splice(i,1);removed=true;
+            break;
+        }
+    }
+    if(!removed)
+    data.push([x,y]);
+    LRModel = calculateLR(); 
 }
