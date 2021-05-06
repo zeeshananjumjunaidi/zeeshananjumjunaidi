108b5a2e517588d4ade6ff1b41b6f4155c1141a1
Zeeshan Anjum <zeeshananjumjunaidi@gmail.com>
2021-05-06 15:22:36 +0300

    init parking setup

---
+++ autonomous_parking/main.js
@@ -0,0 +1,16 @@
+
+
+
+
+var width = 10;
+var height = 10;
+
+function setup(){
+    width = window.innerWidth;
+    height = window.innerHeight;
+    createCanvas(width,height);
+}
+
+function draw(){
+    background(200);
+}
---
+++ autonomous_parking/parking.js
@@ -0,0 +1,6 @@
+class Parking{
+
+    constructor(frontCarLine,backCarLine,sideCarLine,vehicle){
+        
+    }
+}
