f2bde9a79d49c36e33f8a36873ab34a47b7afb14
Zeeshan Anjum <zeeshananjumjunaidi@gmail.com>
2021-05-16 16:43:04 +0300

    update radar and target

--- radar/radar.js
+++ radar/radar.js
@@ -3,13 +3,19 @@
 var width=1;
 var height=1;
 var mainColor;
-var sector_radius=200;
+var radar={
+    angle:0,
+    range:400
+}
+var targets=[];
+
 function setup(){
     width = window.innerWidth;
     height=window.innerHeight;
     createCanvas(width,height);
     mainColor = color(40,250,10);
     sector_radius=width*0.1;
+    targets.push(new Target(0,0,Math.PI/4,2));
 }
 
 function draw(){
@@ -20,11 +26,36 @@ function draw(){
     line(width/2,0,width/2,height);
     push();
     translate(width/2,height/2);
-    circle(0,0,sector_radius);
-    circle(0,0,2*sector_radius);
-    circle(0,0,3*sector_radius);
-    circle(0,0,3.5*sector_radius);
-    circle(0,0,4*sector_radius);
-    
+    drawBackground();
+    drawScanner();
     pop();
+    drawTargets();
+    if(radar.angle>2*Math.PI){radar.angle=0;}else{radar.angle+=0.001*deltaTime;}
+}
+function drawTargets(){
+    noStroke();
+    fill(255,0,0,150);
+    for(let i=0;i<targets.length;i++){
+        targets[i].draw();
+    }
+}
+function drawScanner(){
+   
+    for(let i=0;i<Math.PI*2;i+=0.1){
+        stroke(40,250,10,50);
+        line(0,0,Math.cos(i)*radar.range,Math.sin(i)*radar.range);
+        fill(40,250,10,150);
+        noStroke();
+        text(i.toFixed(2),Math.cos(i)*radar.range,Math.sin(i)*radar.range);
+    }
+    stroke(40,250,10,120);
+    line(0,0,Math.cos(radar.angle)*radar.range,Math.sin(radar.angle)*radar.range);
+  
+}
+
+function drawBackground(){
+    circle(0,0,radar.range*0.5);
+    circle(0,0,radar.range);
+    circle(0,0,radar.range*1.5);
+    circle(0,0,radar.range*2);
 }
