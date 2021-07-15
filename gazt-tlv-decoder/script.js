278b7952358b4bedee4c5dd63ab9f1df6bac963b
Zeeshan Anjum Junaidi <zeeshananjumjunaidi@gmail.com>
2021-07-15 16:15:16 +0300

    changed style

--- gazt-tlv-decoder/script.js
+++ gazt-tlv-decoder/script.js
@@ -1,22 +1,22 @@
 if (window.addEventListener) {
     window.addEventListener('load', GetOutput);
- } else if (window.attachEvent) {
+} else if (window.attachEvent) {
     window.attachEvent('onload', GetOutput);
- } else { 
+} else {
     window.onload = GetOutput;
- }
+}
 
-function GetOutput(){
-    console.log("Getting Output")
-    let input =document.querySelector('#qr_data');
+function GetOutput() {
+    let input = document.querySelector('#qr_data');
     let output = document.querySelector('#output')
     let b64Data = input.value;
     let outputData = '';
-
-    outputData= parseTLVData(_base64ToArrayBuffer(b64Data));
-    output.innerHTML = outputData;
-    console.log(outputData);
-
+    if (b64Data) {
+        outputData = parseTLVData(_base64ToArrayBuffer(b64Data));
+        output.innerHTML = outputData;
+    } else {
+        output.innerHTML ='<b style="color:crimson;">Please enter base64 Data from QR Code</b>';
+    }
 }
 function _base64ToArrayBuffer(base64) {
     var binary_string = window.atob(base64);
@@ -27,28 +27,25 @@ function _base64ToArrayBuffer(base64) {
     }
     return bytes;
 }
-function parseTLVData(bytesArray){
-    data='<table><tr><th>Tag</th><th>Length</th><th>Value</th></tr>';
-    start=true;
-    // for(let i=0;i<bytesArray.length;i++){
-        let i=0;
-        while(i<bytesArray.length){
-        let b =bytesArray[i];
+function parseTLVData(bytesArray) {
+    data = '<table><tr><th>Tag</th><th>Length</th><th>Value</th></tr>';
+    start = true;
+    let i = 0;
+    while (i < bytesArray.length) {
+        let b = bytesArray[i];
         data += `<tr><td>${b}</td>`;
         i++;
         let tagLength = bytesArray[i];
         data += `<td>${tagLength}</td>`;
-        //data+= ` Value Length: ${tagLength}`;
         i++;
-        let value =uintToString(bytesArray.slice(i,i+tagLength+2));
-        i+=tagLength;
-        //data+=` Value: ${value}`;
+        let value = uintToString(bytesArray.slice(i, i + tagLength + 2));
+        i += tagLength;
         data += `<td>${value}</td></tr>`;
 
-   
+
     }
-    data+='</table>';
-return data;
+    data += '</table>';
+    return data;
 }
 function uintToString(uintArray) {
     var encodedString = String.fromCharCode.apply(null, uintArray),
