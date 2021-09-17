af4f48f8e46e7ee2180d6546fdb49cde65c917e7
Zeeshan Anjum Junaidi <zeeshananjumjunaidi@gmail.com>
2021-09-18 04:59:18 +0500

    setup gravity and orbit

--- gravity_assist/script.js
+++ gravity_assist/script.js
@@ -7,21 +7,32 @@ var gravitySource;
 var spacecraft;
 var width;
 var height;
+var isPause = false;
 function setup() {
     width = window.innerWidth;
     height = window.innerHeight;
     createCanvas(width, height);
     rectMode(CENTER)
 
-    gravitySource = new GravitySource(0, 0, 10e4, 100);
-    spacecraft = new Spacecraft(-width/2.5, 0, 10, 20);
+    gravitySource = new GravitySource(0, 0, 11000, 100);
+    spacecraft = new Spacecraft(-width / 2.5, 0, 100, 20);
+    $('#playBtn').click(()=> {
+        console.log(this.isPause)
+        this.isPause = !this.isPause;
+        if (this.isPause) { $('#playBtn')[0].innerHTML = '<i class="fas fa-pause"></i>' } else {
+            $('#playBtn')[0].innerHTML = '<i class="fas fa-play"></i>';
+        }
+    });
 }
 
 function draw() {
-    translate(width/2,height/2);
+    translate(width / 2, height / 2);
     background(255);
     noStroke();
     fill(0);
     gravitySource.draw();
     spacecraft.draw();
+    if(!isPause){
+        spacecraft.simulate(gravitySource);
+    }
 }
--- gravity_assist/spacecraft.js
+++ gravity_assist/spacecraft.js
@@ -1,13 +1,56 @@
 
 class Spacecraft {
-    constructor(x, y, mass, sizeInRadius) {
+    constructor(x, y, mass, sizeInRadius, vx = 0.01, vy = 0.200001) {
         this.x = x;
         this.y = y;
         this.mass = mass;
         this.sizeInRadius = sizeInRadius;
+        this.w = window.innerWidth / 2;
+        this.h = window.innerHeight / 2;
+        this.vel = new p5.Vector(vx, vy);
     }
     draw() {
         fill(0, 0, 255);
         circle(this.x, this.y, this.sizeInRadius);
+        this.keepInbound();
+    }
+    /*
+    gs: Gravity Source (i.e. Planet or any cellestial body)
+    */
+    simulate(gs) {
+        let d = dist(this.x, this.y, gs.x, gs.y);
+        let g = 6.67 * (this.mass + gs.mass) / d ** 2;
+        this.vel.add(g * (gs.x - this.x) / d / this.mass, g * (gs.y - this.y) / d / this.mass);
+        this.x += deltaTime * this.vel.x;
+        this.y += deltaTime * this.vel.y;
+        this.drawSimulationPath(gs);
+    }
+    drawSimulationPath(gs) {
+        let _x = this.x;
+        let _y = this.y;
+        let _vel = this.vel.copy();
+        push();
+        stroke(0)
+        for (let i = 0; i < 200; i++) {
+            let d = dist(_x, _y, gs.x, gs.y);
+            let g = 6.67 * (this.mass + gs.mass) / d ** 2;
+            _vel.add(g * (gs.x - _x) / d / this.mass, g * (gs.y - _y) / d / this.mass);
+            _x += deltaTime * _vel.x;
+            _y += deltaTime * _vel.y;
+            point(_x,_y);
+        }
+        pop();
+    }
+
+    keepInbound() {
+        fill(0)
+        if (this.x < -this.w) { this.x = this.w * 2; }
+        else if (this.x > this.w * 2) {
+            this.x = -this.w;
+        }
+        if (this.y < -this.h) { this.y = this.h * 2; }
+        else if (this.y > this.h * 2) {
+            this.y = -this.h;
+        }
     }
 }
