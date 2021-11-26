eaeb78f69c552ef85929ca480a897d53f44f2f1a
Zeeshan Anjum Junaidi <zeeshananjumjunaidi@gmail.com>
2021-11-27 04:39:03 +0500

    upgraded new UI, fix some minor issues related to shadows

--- x-rotor/helper.js
+++ x-rotor/helper.js
@@ -4,15 +4,13 @@
 function createCircle() {
     var radius = 1,
         segments = 64,
-        material = new THREE.LineBasicMaterial({ color: 0x0000ff }),
+        material = new THREE.LineBasicMaterial({ color: 0xffffff }),
         geometry = new THREE.CircleGeometry(radius, segments);
 
     // Remove center vertex
     geometry.vertices.shift();
-    
+
     // Non closed circle with one open segment:
     // scene.add(new THREE.Line(geometry, material));
-
-    // To get a closed circle use LineLoop instead (see also @jackrugile his comment):
     return new THREE.LineLoop(geometry, material);
 }
--- x-rotor/script.js
+++ x-rotor/script.js
@@ -1,8 +1,31 @@
+
+class Quadcopter {
+    constructor(position, quaternion) {
+        this.position = position;
+        this.quaternion = quaternion;
+        this.isEngineStart = false;
+    }
+    start() {
+        this.isEngineStart = true;
+    }
+    stop() {
+        this.isEngineStart = false;
+    }
+    toggleEngine() {
+        this.isEngineStart = !this.isEngineStart;
+    }
+}
+
 (function () {
+
+    //  --------------- System State -------------
+    var isPlaying = true;
+
+    //  --------------- Quadcopter ---------------
+    var quadcopter = new Quadcopter();
+    quadcopter.start();
     var vel = new CANNON.Vec3(0, 1, 0);
     var targetPosition = new CANNON.Vec3(0, 5, 0);
-
-
     // var pid = new PID(0.01, 0.00001, 0.0001, t);
 
     var PID_pitch = new PIDController();
@@ -15,6 +38,33 @@
     // var PID_pitch_gains = new CANNON.Vec3(0.01, 0.00001, 0.0001); //(2, 3, 2)
     var throttle = 0;
     var forward = 0;
+    let engineBtn = document.querySelector('#engineBtn');
+    let playpauseBtn = document.querySelector('#playpauseBtn');
+    let resetBtn = document.querySelector('#resetBtn');
+    let pauseScreen = document.querySelector('#pauseScreen');
+    resetBtn.addEventListener('click', () => {
+        location.reload();
+    });
+    playpauseBtn.addEventListener('click', () => {
+        isPlaying = !isPlaying;
+        if (isPlaying) {
+            animate();
+             pauseScreen.classList.remove('disabled');
+            playpauseBtn.innerHTML = `Pause <i class="fas fa-pause"></i>`;
+        } else {
+            pauseScreen.classList.add('disabled');
+            playpauseBtn.innerHTML = `Play <i class="fas fa-play"></i>`;
+        }
+    });
+    engineBtn.addEventListener('click', () => {
+        quadcopter.toggleEngine();
+        if (quadcopter.isEngineStart) {
+            engineBtn.innerHTML = 'Turn Off <i class="fas fa-power-off"></i>';
+        } else {
+            engineBtn.innerHTML = 'Turn On <i class="fas fa-power-off"></i>';
+        }
+    });
+
     document.addEventListener('keydown', function (ev) {
         const up = ['ArrowUp', 'w'];
         const down = ['ArrowDown', 's'];
@@ -22,14 +72,18 @@
         const right = ['ArrowRight', 'd'];
         if (up.includes(ev.key)) {
             targetPosition.y += delta * 100;
+            targetPosition.y = Math.min(targetPosition.y, 100);
             // vel.y += 0.1;
         } else if (down.includes(ev.key)) {
             targetPosition.y -= delta * 100;
+            targetPosition.y = Math.max(targetPosition.y, 0);
             // vel.y -= 0.1;
         } else if (left.includes(ev.key)) {
             targetPosition.x += delta * 100;
         } else if (right.includes(ev.key)) {
             targetPosition.x -= delta * 100;
+        } else if (ev.key == 'e') {
+            quadcopter.isEngineStart = !quadcopter.isEngineStart;
         }
     });
 
@@ -39,6 +93,8 @@
 
     var robot;
     const robotBody = new CANNON.Body({ mass: 10 });
+    console.log(robotBody)
+    window.robotBody = robotBody;
     var rotors = [];
     function loadQuadCopter(sceneRef) {
         const loader = new THREE.FBXLoader();
@@ -52,7 +108,7 @@
 
         loader.load("./models/rotor-model.fbx", model => {
             robot = model;
-            robot.position.y = 1.5;
+            robot.position.y = 5;
             model.scale.set(1, 1, 1);
             // robot.add(camera);
             //robot.add(light);
@@ -63,6 +119,9 @@
             robotBody.position.x = model.position.x;
             robotBody.position.y = model.position.y;
             robotBody.position.z = model.position.z;
+            robotBody.quaternion.set(model.quaternion.x,
+                model.quaternion.y, model.quaternion.z, model.quaternion.w);
+
             world.addBody(robotBody);
 
             // const pointLight = new THREE.PointLight(0xff0000, 4, 100);
@@ -72,30 +131,11 @@
             // const pointLightHelper = new THREE.PointLightHelper(pointLight, sphereSize);
             // sceneRef.add(pointLightHelper);
             //const rotorMtl = new THREE.MeshPhongMaterial({ color: 0xeeeeee, side: THREE.DoubleSide });
+            let materialSet = {};
             model.traverse(function (child) {
                 if (child instanceof THREE.Mesh) {
                     child.castShadow = true; //default is false
-                    // child.receiveShadow = true; //default
-                    let m = child.material;// = new THREE.MeshPhongMaterial({ color: 0xeeeeee, side: THREE.DoubleSide });;
-                    // m.color='0xeeeeee';
-                    // m.flatShading=true;
-                    // m.side = 2;
-                    if (m.name == 'MetalMtl') {
-                        m.map = metal;
-                    }
-                    else if (m.name == 'primaryMtl') {
-                        m.map = primary;
-                    }
-                    else if (m.name == 'secondaryMtl') {
-                        m.map = secondary;
-                    }
-                    else if (m.name == 'LensMtl') {
-                        m.map = lens;
-                    }
-                    else if (m.name == 'RubberMtl') {
-                        m.map = rubber;
-                    }
-
+                    materialSet[child.material.name] = child.material;
                 }
 
                 else if (child instanceof THREE.Group) {
@@ -110,8 +150,36 @@
                         }
                     });
                 }
-
             });
+            window.materialSet = materialSet;
+            for (let i in materialSet) {
+                let m = materialSet[i];
+
+                m.side = 0;
+                m.color.r = 2;
+                m.color.g = 2;
+                m.color.b = 2;
+                m.shineness = 1000;
+                m.specular.r = 0.1;
+                m.specular.g = 0.1;
+                m.specular.b = 0.1;
+                if (m.name == 'MetalMtl') {
+                    m.map = metal;
+                }
+                else if (m.name == 'primaryMtl') {
+                    m.map = primary;
+                    window.primary = m;
+                }
+                else if (m.name == 'secondaryMtl') {
+                    m.map = secondary;
+                }
+                else if (m.name == 'LensMtl') {
+                    m.map = lens;
+                }
+                else if (m.name == 'RubberMtl') {
+                    m.map = rubber;
+                }
+            }
             sceneRef.add(model);
         });
     }
@@ -144,7 +212,7 @@
     const light = new THREE.DirectionalLight(0xffffff, 1.0);
     light.position.set(0, 100, 0);
     light.target.position.set(111, 10, 50);
-    light.castShadow = true; 
+    light.castShadow = true;
 
     // -------------- Light Shadow ---------------
     //Set up shadow properties for the light
@@ -188,25 +256,28 @@
     planeBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), -Math.PI / 2)
     world.addBody(planeBody)
 
-    
+
     // -------------- Load Quadcopter ------------
     loadQuadCopter(scene);
 
     // -------------- Animation ------------------
     const clock = new THREE.Clock()
     let delta;
-    const animate = function () {        
+    const animate = function () {
         delta = Math.min(clock.getDelta(), 0.1)
         world.step(delta)
-
-        requestAnimationFrame(animate);
-        if (rotors.length > 0) {
-            for (let i = 0; i < rotors.length; i++) {
-                rotors[i].rotation.z += delta * 50 * vel.length();
-            }
+        if (isPlaying)
+            requestAnimationFrame(animate);
+        if (quadcopter.isEngineStart && rotors.length > 0) {
+            if (robotBody.velocity.length() > 0)
+                for (let i = 0; i < rotors.length; i++) {
+                    rotors[i].rotation.z += delta * 50 * vel.length();
+                }
         }
         if (robot && robotBody) {
             robot.position.set(robotBody.position.x, robotBody.position.y, robotBody.position.z);
+            robot.quaternion.set(robotBody.quaternion.x, robotBody.quaternion.y,
+                robotBody.quaternion.z, robotBody.quaternion.w);
             planeObject.position.set(robot.position.x, 0, robot.position.z);
             robotBody.velocity.x = vel.x;
 
@@ -221,10 +292,10 @@
             let forwardError = getForwardError(robotBody.position, targetPosition);
             let PID_forward_output = PID_forward.GetFactorFromPIDController(PID_forward_gains_adapted, forwardError);
 
-
-            robotBody.velocity.y += PID_pitch_output * 9.8 * 10;
-            robotBody.velocity.x += PID_forward_output * 9.8 * 110;
-
+            if (quadcopter.isEngineStart) {
+                robotBody.velocity.y += PID_pitch_output * 9.8 * 10;
+                robotBody.velocity.x += PID_forward_output * 9.8 * 110;
+            }
             targetCircle.position.set(targetPosition.x, targetPosition.y, targetPosition.z);
             light.position.set(robot.position.x, light.position.y, robot.position.z);
             light.target.position.set(robot.position.x, light.target.position.y, robot.position.z);
@@ -233,7 +304,7 @@
         controls.update();
         renderer.render(scene, camera);
     };
-    
+
     function getPitchError(current, target) {
         return target.y - current.y;
     }
